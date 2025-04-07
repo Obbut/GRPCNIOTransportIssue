741dca56c426386da5fa0469e12cc16d85c7dea9
Robbert Brandsma <i@robbertbrandsma.nl>
2025-04-07 13:05:43 +0200

    Update Call-Server script

--- Scripts/Call-Server.sh
+++ Scripts/Call-Server.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-grpcurl -protoset=Sources/HelloWorldProto/hello_world.protoset \
-    -plaintext \
-    -d '{"name": "World"}' \
-    127.0.0.1:8080 \
-    helloworld.Greeter/SayHello
+for i in {1..100}; do
+    for i in {1..100}; do
+        grpcurl -protoset=Sources/HelloWorldProto/hello_world.protoset \
+            -plaintext \
+            -d '{"name": "World"}' \
+            127.0.0.1:8080 \
+            helloworld.Greeter/SayHello
+    done
+    wait
+done
